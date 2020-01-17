bc56d0ed0a0b0b5b1c744c956fb99b8b4384ecd7
Peter-32 <peter@impactradius.com>
2020-01-16 20:11:08 -0800

    a

--- src/main/employee_network_plot.py
+++ src/main/employee_network_plot.py
@@ -1,5 +1,6 @@
 # Imports and initialize dictionaries
 #### Imports
+import sys
 import json
 import networkx as nx
 from datetime import datetime
@@ -7,86 +8,20 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 plt.rc('figure', figsize=[16,16])
 #### Initialize dictionaries
-location_mapping = {'US-OH-Columbus': 'Columbus',
- 'US-NY-New York': 'New York',
- 'US-CA-Remote (San Francisco)': 'Remote',
- 'AU-Sydney': 'Sydney',
- 'ZA-Cape Town': 'Cape Town',
- 'US-TN-Remote (Santa Barbara)': 'Remote',
- 'US-NY (Remote) UK': 'Remote',
- 'US-WA-Seattle': 'Seattle',
- 'EU-Poland': 'Remote',
- 'US-CA-San Francisco': 'Remote',
- 'US-NC-Remote (New York)': 'Remote',
- 'US-CA-Remote (Santa Barbara)': 'Remote',
- 'SG-Singapore': 'Singapore',
- 'US-TX-Remote (Seattle)': 'Remote',
- '0. Advisor - US': 'Remote',
- 'US-OH-Remote (Columbus)': 'Remote',
- 'US-AZ-Remote (New York)': 'Remote',
- 'US-AR-Remote (Columbus)': 'Remote',
- 'NO-Oslo': 'Remote',
- 'US-CO-Remote (New York)': 'Remote',
- 'US-CA-Remote (Seattle)': 'Remote',
- 'CN-Shanghai': 'Shanghai',
- 'US-AZ-Remote (Seattle)': 'Remote',
- 'US-CT-Remote (New York)': 'Remote',
- '0.Contractor': 'Remote',
- 'GB-London': 'London',
- 'US-CA-Santa Barbara': 'Santa Barbara',
- 'Nepal (US-WA-Seattle': 'Seattle',
- 'US-HI-Remote (Santa Barbara)': 'Remote',
- }
-department_color_mapping = {
-    'Product Management': '#b5a265', # Dark brown
-    'Sales': '#d43a2f', # Red
-    'None': '#b5abaa', # Gray
-    'G&A': '#150782', # Dark blue
-    'Finance': '#17cf0a', # Green
-    'Support and Operations': '#faf619', # Yellow
-    'Marketing': '#ffb700', # Orange
-    'Technology': '#07deed', # Light blue
-    'People': '#e3d698' # Beige (light brown)
-}
-location_color_mapping = {
-    'Singapore': '#b5a265', # Dark brown
-    'Columbus': '#d43a2f', # Red
-    'Seattle': '#b5abaa', # Gray
-    'Shanghai': '#150782', # Dark blue
-    'New York': '#17cf0a', # Green
-    'London': '#faf619', # Yellow
-    'Santa Barbara': '#ffb700', # Orange
-    'Cape Town': '#07deed', # Light blue
-    'Remote': '#e3d698', # Beige (light brown)
-    'Sydney': '#bf08fc' # Purple
-}
+sys.path.append('/Users/petermyers/Desktop/New_Code/employee_network_plot/src/main')
+from employeemappings.employeemappings import Config
 
 def draw_graph(year='2019', color_by='department'):
     # Initialize
     G = nx.Graph(k=0.8,iterations=50)
     job_titles, locations, names, department_names, edge_list = [], [], [], [], []
     date = datetime.strptime('{}-12-31'.format(year), '%Y-%m-%d')
-    department_nodes = {'Product Management': [],
-     'Sales': [],
-     'None': [],
-     'G&A': [],
-     'Finance': [],
-     'Support and Operations': [],
-     'Marketing': [],
-     'Technology': [],
-     'People': []
-     }
-    location_nodes = {'Singapore': [],
-     'Columbus': [],
-     'Seattle': [],
-     'Shanghai': [],
-     'New York': [],
-     'London': [],
-     'Santa Barbara': [],
-     'Cape Town': [],
-     'Remote': [],
-     'Sydney': []
-     }
+    department_nodes = {}
+    for department in Config.departments:
+        department_nodes[department] = []
+    location_nodes = {}
+    for location in Config.location_nodes:
+        location_nodes[location] = []
 
     # Read file
     with open('../../data/raw/data.json') as json_file:
@@ -95,11 +30,11 @@ def draw_graph(year='2019', color_by='department'):
         for person1 in data['directReports']:
             if date > datetime.strptime(person1['hireDate'], '%Y-%m-%d'):
                 job_titles.append(person1['jobTitle'])
-                locations.append(location_mapping[person1['location']])
+                locations.append(Config.location_mapping[person1['location']])
                 department_names.append(str(person1['departmentName']))
                 G.add_node(person1['name'])
                 department_nodes[str(person1['departmentName'])].append(person1['name'])
-                location_nodes[location_mapping[person1['location']]].append(person1['name'])
+                location_nodes[Config.location_mapping[person1['location']]].append(person1['name'])
                 G.add_edge(person1['name'], "Top position")
                 person1_name = person1['name']
             else:
@@ -108,11 +43,11 @@ def draw_graph(year='2019', color_by='department'):
             for person2 in person1['directReports']:
                 if date > datetime.strptime(person2['hireDate'], '%Y-%m-%d'):
                     job_titles.append(person2['jobTitle'])
-                    locations.append(location_mapping[person2['location']])
+                    locations.append(Config.location_mapping[person2['location']])
                     department_names.append(str(person2['departmentName']))
                     G.add_node(person2['name'])
                     department_nodes[str(person2['departmentName'])].append(person2['name'])
-                    location_nodes[location_mapping[person2['location']]].append(person2['name'])
+                    location_nodes[Config.location_mapping[person2['location']]].append(person2['name'])
                     G.add_edge(person2['name'], person1_name)
                     person2_name = person2['name']
                 else:
@@ -121,11 +56,11 @@ def draw_graph(year='2019', color_by='department'):
                 for person3 in person2['directReports']:
                     if date > datetime.strptime(person3['hireDate'], '%Y-%m-%d'):
                         job_titles.append(person3['jobTitle'])
-                        locations.append(location_mapping[person3['location']])
+                        locations.append(Config.location_mapping[person3['location']])
                         department_names.append(str(person3['departmentName']))
                         G.add_node(person3['name'])
                         department_nodes[str(person3['departmentName'])].append(person3['name'])
-                        location_nodes[location_mapping[person3['location']]].append(person3['name'])
+                        location_nodes[Config.location_mapping[person3['location']]].append(person3['name'])
                         G.add_edge(person3['name'], person2_name)
                         person3_name = person3['name']
                     else:
@@ -134,11 +69,11 @@ def draw_graph(year='2019', color_by='department'):
                     for person4 in person3['directReports']:
                         if date > datetime.strptime(person4['hireDate'], '%Y-%m-%d'):
                             job_titles.append(person4['jobTitle'])
-                            locations.append(location_mapping[person4['location']])
+                            locations.append(Config.location_mapping[person4['location']])
                             department_names.append(str(person4['departmentName']))
                             G.add_node(person4['name'])
                             department_nodes[str(person4['departmentName'])].append(person4['name'])
-                            location_nodes[location_mapping[person4['location']]].append(person4['name'])
+                            location_nodes[Config.location_mapping[person4['location']]].append(person4['name'])
                             G.add_edge(person4['name'], person3_name)
                             person4_name = person4['name']
                         else:
@@ -147,11 +82,11 @@ def draw_graph(year='2019', color_by='department'):
                         for person5 in person4['directReports']:
                             if date > datetime.strptime(person5['hireDate'], '%Y-%m-%d'):
                                 job_titles.append(person5['jobTitle'])
-                                locations.append(location_mapping[person5['location']])
+                                locations.append(Config.location_mapping[person5['location']])
                                 department_names.append(str(person5['departmentName']))
                                 G.add_node(person5['name'])
                                 department_nodes[str(person5['departmentName'])].append((person5['name']))
-                                location_nodes[location_mapping[person5['location']]].append(person5['name'])
+                                location_nodes[Config.location_mapping[person5['location']]].append(person5['name'])
                                 G.add_edge(person5['name'], person4_name)
                                 person5_name = person5['name']
                             else:
@@ -163,54 +98,54 @@ def draw_graph(year='2019', color_by='department'):
     if color_by == "location":
         for location in location_nodes.keys():
             for person_name in location_nodes[location]:
-                G.nodes[person_name]['color'] = location_color_mapping[location]
-        legend_elements = [Line2D([0], [0], marker='o', color='#b5a265', label='Singapore',
+                G.nodes[person_name]['color'] = Config.location_color_mapping[location]
+        legend_elements = [Line2D([0], [0], marker='o', color='#b5a265', label='Location 1',
                                   markerfacecolor='#b5a265', markersize=15),
-                           Line2D([0], [0], marker='o', color='#d43a2f', label='Columbus',
+                           Line2D([0], [0], marker='o', color='#d43a2f', label='Location 2',
                                   markerfacecolor='#d43a2f', markersize=15),
-                           Line2D([0], [0], marker='o', color='#b5abaa', label='Seattle',
+                           Line2D([0], [0], marker='o', color='#b5abaa', label='Location 3',
                                   markerfacecolor='#b5abaa', markersize=15),
-                           Line2D([0], [0], marker='o', color='#150782', label='Shanghai',
+                           Line2D([0], [0], marker='o', color='#150782', label='Location 4',
                                   markerfacecolor='#150782', markersize=15),
-                           Line2D([0], [0], marker='o', color='#17cf0a', label='New York',
+                           Line2D([0], [0], marker='o', color='#17cf0a', label='Location 5',
                                   markerfacecolor='#17cf0a', markersize=15),
-                           Line2D([0], [0], marker='o', color='#faf619', label='London',
+                           Line2D([0], [0], marker='o', color='#faf619', label='Location 6',
                                   markerfacecolor='#faf619', markersize=15),
-                           Line2D([0], [0], marker='o', color='#ffb700', label='Santa Barbara',
+                           Line2D([0], [0], marker='o', color='#ffb700', label='Location 7',
                                   markerfacecolor='#ffb700', markersize=15),
-                           Line2D([0], [0], marker='o', color='#07deed', label='Cape Town',
+                           Line2D([0], [0], marker='o', color='#07deed', label='Location 8',
                                   markerfacecolor='#07deed', markersize=15),
-                           Line2D([0], [0], marker='o', color='#e3d698', label='Remote',
+                           Line2D([0], [0], marker='o', color='#e3d698', label='Location 9',
                                   markerfacecolor='#e3d698', markersize=15),
-                           Line2D([0], [0], marker='o', color='#bf08fc', label='Sydney',
+                           Line2D([0], [0], marker='o', color='#bf08fc', label='Location 10',
                                   markerfacecolor='#bf08fc', markersize=15)]
     else:
         for department in department_nodes.keys():
             for person_name in department_nodes[department]:
-                G.nodes[person_name]['color'] = department_color_mapping[department]
-        legend_elements = [Line2D([0], [0], marker='o', color='#b5a265', label='Product Management',
+                G.nodes[person_name]['color'] = Config.department_color_mapping[department]
+        legend_elements = [Line2D([0], [0], marker='o', color='#b5a265', label='Department 1',
                                   markerfacecolor='#b5a265', markersize=15),
-                           Line2D([0], [0], marker='o', color='#d43a2f', label='Sales',
+                           Line2D([0], [0], marker='o', color='#d43a2f', label='Department 2',
                                   markerfacecolor='#d43a2f', markersize=15),
-                           Line2D([0], [0], marker='o', color='#b5abaa', label='None',
+                           Line2D([0], [0], marker='o', color='#b5abaa', label='Department 3',
                                   markerfacecolor='#b5abaa', markersize=15),
-                           Line2D([0], [0], marker='o', color='#150782', label='G&A',
+                           Line2D([0], [0], marker='o', color='#150782', label='Department 4',
                                   markerfacecolor='#150782', markersize=15),
-                           Line2D([0], [0], marker='o', color='#17cf0a', label='Finance',
+                           Line2D([0], [0], marker='o', color='#17cf0a', label='Department 5',
                                   markerfacecolor='#17cf0a', markersize=15),
-                           Line2D([0], [0], marker='o', color='#faf619', label='Support and Operations',
+                           Line2D([0], [0], marker='o', color='#faf619', label='Department 6',
                                   markerfacecolor='#faf619', markersize=15),
-                           Line2D([0], [0], marker='o', color='#ffb700', label='Marketing',
+                           Line2D([0], [0], marker='o', color='#ffb700', label='Department 7',
                                   markerfacecolor='#ffb700', markersize=15),
-                           Line2D([0], [0], marker='o', color='#07deed', label='Technology',
+                           Line2D([0], [0], marker='o', color='#07deed', label='Department 8',
                                   markerfacecolor='#07deed', markersize=15),
-                           Line2D([0], [0], marker='o', color='#e3d698', label='People',
+                           Line2D([0], [0], marker='o', color='#e3d698', label='Department 9',
                                   markerfacecolor='#e3d698', markersize=15)]
     G.nodes['Top position']['color'] = '#b5abaa'
     colors = [node[1]['color'] for node in G.nodes(data=True)]
 
     # Plot and save
-    nx.draw(G, with_labels=True, node_color=colors)
+    nx.draw(G, with_labels=False, node_color=colors)
     plt.legend(handles=legend_elements, loc='fit')
     plt.savefig('../../data/output/{}_graph_{}.png'.format(color_by, year))
     G.clear()
